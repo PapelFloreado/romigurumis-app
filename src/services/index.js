aa154f9fe56299c5a9dcb41b3f47eeb078f3f169
PapelFloreado <101934170+PapelFloreado@users.noreply.github.com>
2023-03-15 20:08:57 -0300

    romigurumis app v0.5b mp/datos API

--- src/services/index.js
+++ src/services/index.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlVz1F5oSuYguXv3DZrwiYgV71iJxjH8A",
-  authDomain: "romigurumis-app.firebaseapp.com",
-  projectId: "romigurumis-app",
-  storageBucket: "romigurumis-app.appspot.com",
-  messagingSenderId: "1000964735868",
-  appId: "1:1000964735868:web:6ecd7f76c0d655acb4daf9",
-  measurementId: "G-MEN7PEL8L1"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTO_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };      
 
 // Initialize Firebase
